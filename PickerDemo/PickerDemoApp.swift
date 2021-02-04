30cfc11e5a1a2e12719fcabccc7502358d92f605
Dave Reed <dave256@mac.com>
2021-02-04 16:09:19 -0500

    Initial Commit

---
+++ PickerDemo/PickerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickerDemoApp.swift
+//  PickerDemo
+//
+//  Created by David M Reed on 2/4/21.
+//
+
+import SwiftUI
+
+@main
+struct PickerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
